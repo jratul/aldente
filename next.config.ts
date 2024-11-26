2e5898c705cf595cffec1c0c2465407803b90d1f
jratul <jratul@nate.com>
2024-11-26 09:32:35 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,18 +12,7 @@ const nextConfig: NextConfig = {
         hostname: "*.s3.ap-northeast-2.amazonaws.com",
       },
     ],
-  },
-  "headers": [
-    {
-      "source": "/api/(.*)",
-      "headers": [
-        { "key": "Access-Control-Allow-Credentials", "value": "true" },
-        { "key": "Access-Control-Allow-Origin", "value": "*" },
-        { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-      ]
-    }
-  ]
+  }
 };
 
 export default nextConfig;
